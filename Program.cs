b0a508f233c326d0010d9761508d5f7e31e9ff76
Your Name <you@example.com>
2023-07-18 13:00:50 +0500

    Face Detection

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("enter the first side of the push corner : ");
+decimal first = Convert.ToDecimal(Console.ReadLine());
+Console.Write("enter the second side of the push corner : ");
+decimal second = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine($"push corner surface : {first + second}");
