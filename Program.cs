2ed997ba5df5349fd025f91fce9f1432ce3395ee
Your Name <you@example.com>
2023-07-19 21:52:41 +0500

    Update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿Console.Write("enter the first side of the push corner : ");
+﻿Console.Write("enter the first side of the Rectangel corner : ");
 decimal first = Convert.ToDecimal(Console.ReadLine());
-Console.Write("enter the second side of the push corner : ");
+Console.Write("enter the second side of the Rectangel corner : ");
 decimal second = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine($"push corner surface : {first + second}");
+Console.WriteLine($"Rectangel corner surface : {first + second}");
