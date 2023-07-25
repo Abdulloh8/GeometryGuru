a0118f878b4c461bf65f2335f189dd09f3659535
Your Name <you@example.com>
2023-07-25 12:53:58 +0500

    Switch Operation

--- Program.cs
+++ Program.cs
@@ -5,31 +5,29 @@
         static void Main()
         {
             Console.WriteLine("1 Rectangel of surface");
-            Console.WriteLine("1 cubic of volume");
+            Console.WriteLine("2 cubic of volume");
 
             string message = Console.ReadLine();
             int value = Convert.ToInt32(message);
-            if(value == 1)
+            
+            switch(value)
             {
-                Console.Write("enter the first side of the Rectangel corner : ");
-            decimal first = Convert.ToDecimal(Console.ReadLine());
-            Console.Write("enter the second side of the Rectangel corner : ");
-            decimal second = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine($"Rectangel corner surface : {first * second}");
-            }
-            else if(value == 2)
-            {
-                Console.Write("enter the first side of the Rectangel corner : ");
-                decimal first = Convert.ToDecimal(Console.ReadLine());
-                Console.Write("enter the second side of the Rectangel corner : ");
-                decimal second = Convert.ToDecimal(Console.ReadLine());
-                Console.Write("enter the height side of the Rectangel corner : ");
-                decimal height = Convert.ToDecimal(Console.ReadLine());
-                Console.WriteLine($"Rectangel corner surface : {(first * second) * height}");
-            }
-            else
-            {
-                Console.Write("enter the first side of the Rectangel corner : ");
+                case 1: Console.Write("enter the first side of the Rectangel corner : ");
+                        decimal first = Convert.ToDecimal(Console.ReadLine());
+                        Console.Write("enter the second side of the Rectangel corner : ");
+                        decimal second = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine($"Rectangel corner surface : {first * second}");
+                    break;
+                case 2: Console.Write("enter the first side of the Rectangel corner : ");
+                        decimal first_ = Convert.ToDecimal(Console.ReadLine());
+                        Console.Write("enter the second side of the Rectangel corner : ");
+                        decimal second_ = Convert.ToDecimal(Console.ReadLine());
+                        Console.Write("enter the height side of the Rectangel corner : ");
+                        decimal height = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine($"Rectangel corner surface : {(first_ * second_) * height}");   
+                    break;  
+                default :   Console.Write("enter the first side of the Rectangel corner : ");
+                    break;    
             }
         }
     }
